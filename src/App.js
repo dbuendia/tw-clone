5adfac8f418bbe806d772651eddd90279b89f42b
dbuendia <buendia.barcelo@gmail.com>
2022-12-15 21:48:29 +0100

    Let's you add tweets to db and show them on UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from "react";
 import { firestore } from "./firebase";
 
 function App() {
+  // Contenedor de todos los tweets que recibimos de firebase:
   const [tweets, setTweets] = useState([]);
+  // Tweet para enviar a firebase desde el form:
+  const [tweet, setTweet] = useState({ tweet: "", autor: "" });
 
   // Get tweets
   useEffect(() => {
@@ -35,27 +38,80 @@ function App() {
         });
         */
 
-        // Seteamos nuestro estado (array de tweets)
+        // Seteamos nuestro estado (array de tweets que recibimos de firebase)
+        // Sirve para cargar los tweets iniciales que hubiera en la db
         setTweets(tweets);
       });
   }, []);
 
+  // Función genérica para ambos inputs
+  const handleInputChange = (e) => {
+    let nuevoTweet = {
+      ...tweet, // Copiamos el estado anterior del objeto tweet con el spread operator
+      [e.target.name]: e.target.value, // Si en los inputs existe la propiedad que se llame como el valor de name, sustitúyelo por los e.target.value de ese input
+    };
+    // Es decir, vamos actualizando cambio a cambios en los inputs y metiéndolos en un estado para enviar a firebase
+    setTweet(nuevoTweet);
+  };
+
+  const sendTweet = (e) => {
+    // Prevenimos que la página se refresque por defecto al pulsar el botón
+    e.preventDefault();
+    // Enviamos al tweet a la colección deseada
+    let enviarTweet = firestore.collection("tweets").add(tweet);
+    // Ahora lo solicitamos para poder mostrarlo en la UI
+    // El envío a la db devuelve una promesa:
+    let solicitarDocumento = enviarTweet.then((docRef) => {
+      // Rescatamos una referencia al documento, cuya información obtendremos con get
+      return docRef.get();
+    });
+    // Ahora podemos rescatar la información del documento
+    solicitarDocumento.then((doc) => {
+      let nuevoTweet = {
+        tweet: doc.data().tweet,
+        autor: doc.data().autor,
+        id: doc.id,
+      };
+      // Y añadir ese nuevo tweet al array de tweets, junto con el resto que hubiera con el spread operator
+      setTweets([nuevoTweet, ...tweets]);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <ul className="tw-container">
-          {/* Si tweets no está vacío, lo recorremos para mostrarlo por UI */}
-          {tweets
-            ? tweets.map((elem) => {
-                return (
-                  <li className="tw">
-                    {elem.autor}: {elem.tweet}
-                  </li>
-                );
-              })
-            : null}
-        </ul>
-      </header>
+      <form>
+        <textarea
+          name="tweet" // mismo nombre que la propiedad del objeto
+          className="textarea"
+          type="text"
+          placeholder="Escribe un tweet..."
+          value={tweet.tweet}
+          onChange={handleInputChange}
+        />
+        <div className="under-textarea">
+          <input
+            name="autor" // mismo nombre que la key del objeto
+            type="text"
+            placeholder="Autor"
+            value={tweet.autor}
+            onChange={handleInputChange}
+          />
+          <input type="button" value="Enviar" onClick={sendTweet} />
+        </div>
+      </form>
+      <ul className="tw-container">
+        <p>Tweets:</p>
+        {/* Si tweets no está vacío, lo recorremos para mostrarlo por UI */}
+        {tweets
+          ? tweets.map((elem) => {
+              return (
+                <li className="tw">
+                  {elem.autor}: {elem.tweet}
+                </li>
+              );
+            })
+          : null}
+      </ul>
     </div>
   );
 }
