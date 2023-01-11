8eee470abee12e32d9c1892da95dfe6201bcdfd8
dbuendia <buendia.barcelo@gmail.com>
2023-01-11 17:21:21 +0100

    Added profile pic for users in db

--- src/TweetsBody.jsx
+++ src/TweetsBody.jsx
@@ -7,42 +7,48 @@ const TweetsBody = ({ tweets, likeTweet, user, deleteTweet }) => {
         {tweets
           ? tweets.map((elem) => {
               return (
-                <div key={elem.id}>
-                  <img
-                    className="user-profile-pic"
-                    src={elem.photoURL}
-                    referrerPolicy="no-referrer" // Lo añado para que se muestre la img
-                  />
-                  <li className="tw">
-                    <p className="autor">@{elem.autor}</p>
-                    <p className="tweet">{elem.tweet}</p>
+                <>
+                  <div className="photo-tw-container" key={elem.id}>
+                    <img
+                      className="user-profile-pic"
+                      src={elem.photoURL}
+                      referrerPolicy="no-referrer" // Lo añado para que se muestre la img
+                    />
+                    <li className="tw">
+                      <p className="autor">@{elem.autor}</p>
+                      <p className="tweet">{elem.tweet}</p>
 
-                    <div className="tw-actions-container">
-                      <div className="action-container">
-                        <span className="heart" onClick={() => likeTweet(elem)}>
-                          ♡
-                        </span>
-                        <span className="like-counter">
-                          {elem.likes ? elem.likes.length : 0}
-                        </span>
+                      <div className="tw-actions-container">
+                        {/* <span>date</span> */}
+                        <div className="action-container">
+                          <span
+                            className="heart"
+                            onClick={() => likeTweet(elem)}
+                          >
+                            ♡
+                          </span>
+                          <span className="like-counter">
+                            {elem.likes ? elem.likes.length : 0}
+                          </span>
+                        </div>
+
+                        {/* Si el tweet uid coincide con el user uid, mostrar borrar, o else no */}
+                        {elem.uid === user?.uid && (
+                          <>
+                            <div className="action-container">
+                              <span
+                                className="delete"
+                                onClick={() => deleteTweet(elem.id)}
+                              >
+                                X
+                              </span>
+                            </div>
+                          </>
+                        )}
                       </div>
-
-                      {/* Si el tweet uid coincide con el user uid, mostrar borrar, o else no */}
-                      {elem.uid === user?.uid && (
-                        <>
-                          <div className="action-container">
-                            <span
-                              className="delete"
-                              onClick={() => deleteTweet(elem.id)}
-                            >
-                              X
-                            </span>
-                          </div>
-                        </>
-                      )}
-                    </div>
-                  </li>
-                </div>
+                    </li>
+                  </div>
+                </>
               );
             })
           : null}
