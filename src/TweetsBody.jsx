ea78ef2624ed8657fdc0cbac61afeaf487c72ab0
dbuendia <buendia.barcelo@gmail.com>
2023-01-13 18:11:47 +0100

    Adds key

--- src/TweetsBody.jsx
+++ src/TweetsBody.jsx
@@ -27,63 +27,61 @@ const TweetsBody = ({ tweets, likeTweet, user, deleteTweet, loading }) => {
           {tweets
             ? tweets.map((elem) => {
                 return (
-                  <>
-                    <div className="photo-tw-container" key={elem.id}>
-                      <img
-                        className="user-profile-pic"
-                        src={elem.photoURL}
-                        referrerPolicy="no-referrer" // Lo añado para que se muestre la img
-                      />
-                      <li className="tw">
-                        <p className="autor">@{elem.autor}</p>
-                        <p className="tweet">{elem.tweet}</p>
+                  <div className="photo-tw-container" key={elem.id}>
+                    <img
+                      className="user-profile-pic"
+                      src={elem.photoURL}
+                      referrerPolicy="no-referrer" // Lo añado para que se muestre la img
+                    />
+                    <li className="tw">
+                      <p className="autor">@{elem.autor}</p>
+                      <p className="tweet">{elem.tweet}</p>
 
-                        <div className="tw-actions-container">
-                          {/* TODO: Add dates */}
-                          <span className="date">
-                            {new Date(elem.fecha).toLocaleString(
-                              "es-ES",
-                              options
-                            )}
-                          </span>
-                          <div className="action-container">
-                            {/* Si el UID del autor está dentro del array de likes, mostrar rojo */}
-                            {Boolean(
-                              elem?.likes?.find((uid) => uid === user?.uid)
-                            ) ? (
-                              <img
-                                className="heart"
-                                src={redHeart}
-                                onClick={() => likeTweet(elem)}
-                              />
-                            ) : (
-                              <img
-                                className="heart"
-                                src={whiteHeart}
-                                onClick={() => likeTweet(elem)}
-                              />
-                            )}
-                            <span className="like-counter">
-                              {elem?.likes ? elem.likes.length : 0}
-                            </span>
-                          </div>
-
-                          {/* Si el tweet uid coincide con el user uid, mostrar borrar, o else no */}
-                          {elem.uid === user?.uid && (
-                            <>
-                              <div className="action-container">
-                                <img
-                                  className="bin"
-                                  src={bin}
-                                  onClick={() => deleteTweet(elem.id)}
-                                />
-                              </div>
-                            </>
+                      <div className="tw-actions-container">
+                        {/* TODO: Add dates */}
+                        <span className="date">
+                          {new Date(elem.fecha).toLocaleString(
+                            "es-ES",
+                            options
                           )}
+                        </span>
+                        <div className="action-container">
+                          {/* Si el UID del autor está dentro del array de likes, mostrar rojo */}
+                          {Boolean(
+                            elem?.likes?.find((uid) => uid === user?.uid)
+                          ) ? (
+                            <img
+                              className="heart"
+                              src={redHeart}
+                              onClick={() => likeTweet(elem)}
+                            />
+                          ) : (
+                            <img
+                              className="heart"
+                              src={whiteHeart}
+                              onClick={() => likeTweet(elem)}
+                            />
+                          )}
+                          <span className="like-counter">
+                            {elem?.likes ? elem.likes.length : 0}
+                          </span>
                         </div>
-                      </li>
-                    </div>
-                  </>
+
+                        {/* Si el tweet uid coincide con el user uid, mostrar borrar, o else no */}
+                        {elem.uid === user?.uid && (
+                          <>
+                            <div className="action-container">
+                              <img
+                                className="bin"
+                                src={bin}
+                                onClick={() => deleteTweet(elem.id)}
+                              />
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    </li>
+                  </div>
                 );
               })
             : null}
